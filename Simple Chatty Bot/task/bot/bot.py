73fe126a0d8b88b319bf025d25be2dbb2d376b88
Ayman Momin <63975056+Galaxylord-AIM@users.noreply.github.com>
2021-12-05 16:45:19 +0530

    Stage 2/5: Print your name

--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -3,5 +3,6 @@ print('I was created in 2020.')
 print('Please, remind me your name.')
 
 # reading a name
+name = input()
 
-print('What a great name you have, {your_name}!')
+print('What a great name you have,', name, '!')
